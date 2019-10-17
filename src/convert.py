2cadc37fd1a7dd5bde8ab33607247517d83acae1
Ulrich Scheller <mail@ulrich-scheller.de>
2019-10-17 17:15:08 +0200

    Create phone, 7inch and 10inch images. MA-1436

--- src/convert.py
+++ src/convert.py
@@ -1,13 +1,21 @@
 from framed_image import FramedImage
 
+# device_frames: nexus9, nexus6p, nexus5x, pixel2xl, wear, ...
+device_to_frame = {
+    "phone": "pixel2xl",
+    "7inch": "nexus9",
+    "10inch": "nexus9",
+}
+
 
 def main():
-    device_frame = "pixel2xl"  # or nexus9, nexus6p, nexus5x, wear, etc..
-    FramedImage(background_name="frame_dog_paws.png", frame=device_frame, screen_shot="screenshot.png") \
-        .add_text("Zooplus Android App",
-                  "Von überall bequem und schnell durch\nmehr als 8.000 Produkte für Ihr Haustier stöbern",
-                  title_font='fonts/MYRIADPRO-BOLDCOND.otf', text_font='fonts/HelveticaNeueLTPro-LtCn.otf') \
-        .save()
+    for device in ("phone", "7inch", "10inch"):
+        FramedImage(background_name="frame_dog_paws.png", frame=device_to_frame[device],
+                    screen_shot=f"screenshot_{device}.png", output_name=f"output_{device}") \
+            .add_text("BOAHSDFKJHSJKFHDJKSHFKJ",
+                      "Von überall bequem und schnell durch\nmehr als 8.000 Produkte für Ihr Haustier stöbern",
+                      title_font='fonts/MYRIADPRO-BOLDCOND.otf', text_font='fonts/HelveticaNeueLTPro-LtCn.otf') \
+            .save()
 
 
 if __name__ == '__main__':
--- src/framed_image.py
+++ src/framed_image.py
@@ -11,6 +11,7 @@ class FramedImage(object):
                  frame="pixel2xl", screen_shot=None):
         self.background = Image.open(f"backgrounds/{background_name}")
         self.output_name = output_name
+        self.frame = frame
         if screen_shot:
             frame = DeviceFrame(frame)
             self.screen_shot = Image.open(screen_shot)
@@ -19,18 +20,23 @@ class FramedImage(object):
             self.set_device_frame(frame.image)
         self.img_draw = ImageDraw.Draw(self.background)
 
+    def _oversize(self):
+        if self.frame == "nexus9":
+            return 200
+        return 0
+
     def set_device_frame(self, image):
         b_size = self.background.size
         f_size = image.size
-        size = (b_size[0], int(f_size[1] * (b_size[0] / f_size[0])))
+        size = (b_size[0] + self._oversize(), int(f_size[1] * (b_size[0] / f_size[0])) + self._oversize())
         image = image.resize(size, Image.ANTIALIAS)
-        self.background.paste(image, (0, 300), image)
+        self.background.paste(image, (0 - (self._oversize()//2), 450 - (self._oversize()//2)), image)
 
     def add_text(self, title, text, title_font, text_font, title_size=80, text_size=50):
         fnt = ImageFont.truetype(title_font, title_size)
-        self.img_draw.text((64, 64), title, fill='white', font=fnt)
+        self.img_draw.text((64, 128), title, fill='white', font=fnt)
         fnt = ImageFont.truetype(text_font, text_size)
-        self.img_draw.text((64, 64 + title_size), text, fill='white', font=fnt, spacing=20)
+        self.img_draw.text((64, 128 + 32 + title_size), text, fill='white', font=fnt, spacing=20)
         return self
 
     def save(self):
