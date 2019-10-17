92d1a7db1b15999febd0772a3cb4be9d7e2bbca1
Ulrich Scheller <mail@ulrich-scheller.de>
2019-10-17 11:53:58 +0200

    Code cleanup. MA-1436

--- src/convert.py
+++ src/convert.py
@@ -26,46 +26,46 @@ class DeviceFrame(object):
     def set_screenshot(self, screenshot):
         boundaries = self._screen_boundaries()
         size = (boundaries[2] - boundaries[0], boundaries[3] - boundaries[1])
-        screenshot = screenshot.resize(size)
+        screenshot = screenshot.resize(size, Image.ANTIALIAS)
         self.image.paste(screenshot, boundaries[:2])
 
 
 class FramedImage(object):
-    def __init__(self, background_name="default.png", output_name="output", size=None):
+    def __init__(self, background_name="default.png", output_name="output",
+                 frame="pixel2xl", screenshot=None):
         self.background = Image.open(f"backgrounds/{background_name}")
-        if size:
-            self.background = self.background.resize(size)
         self.output_name = output_name
+        if screenshot:
+            frame = DeviceFrame(frame)
+            self.screenshot = Image.open(screenshot)
+            frame.set_screenshot(self.screenshot)
+            self.background = self.background.resize(self.screenshot.size)
+            self.set_device_frame(frame.image)
         self.img_draw = ImageDraw.Draw(self.background)
 
     def set_device_frame(self, image):
         b_size = self.background.size
         f_size = image.size
         size = (b_size[0], int(f_size[1] * (b_size[0] / f_size[0])))
-        image = image.resize(size)
-        self.background.paste(image, (0, 400), image)
+        image = image.resize(size, Image.ANTIALIAS)
+        self.background.paste(image, (0, 300), image)
 
     def add_text(self, title, text):
-        title_size = 60
+        title_size = 80
+        text_size = 50
         fnt = ImageFont.truetype('fonts/MYRIADPRO-BOLDCOND.otf', title_size)
         self.img_draw.text((64, 64), title, fill='white', font=fnt)
-        fnt = ImageFont.truetype('fonts/HelveticaNeueLTPro-LtCn.otf', 40)
-        self.img_draw.text((64, 64 + title_size), text, fill='white', font=fnt)
+        fnt = ImageFont.truetype('fonts/HelveticaNeueLTPro-LtCn.otf', text_size)
+        self.img_draw.text((64, 64 + title_size), text, fill='white', font=fnt, spacing=20)
 
     def save(self):
         self.background.save(f"{self.output_name}.png")
 
 
 if __name__ == '__main__':
-    screenshot = Image.open("screenshot.png")
-    # frame = DeviceFrame("nexus6p")
-    # frame = DeviceFrame("nexus5x")
-    frame = DeviceFrame("pixel2xl")
-    # frame = DeviceFrame("nexus9")
-    frame.set_screenshot(screenshot)
+    device_frame = "pixel2xl"  # or nexus9, nexus6p, nexus5x, wear, etc..
 
-    framed_image = FramedImage(background_name="frame_dog_paws.png", size=screenshot.size)
-    framed_image.set_device_frame(frame.image)
+    framed_image = FramedImage(background_name="frame_dog_paws.png", frame=device_frame, screenshot="screenshot.png")
     framed_image.add_text("Zooplus Android App",
                           "Von überall bequem und schnell durch\nmehr als 8.000 Produkte für Ihr Haustier stöbern")
     framed_image.save()
